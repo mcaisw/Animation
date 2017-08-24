f7d86716894d3873e2fdca4dea8070571962f1b2
MengChi <mc2311362372@gmail.com>
2017-08-24 14:12:41 +0800

    ··

---
+++ Assets/Script/TestAniamtionEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAniamtionEvent : MonoBehaviour {
+    AnimationClip clip;
+    AnimationEvent even;
+    Animation anim;
+    AnimationState state;
+	// Use this for initialization
+	void Start () {
+        even = new AnimationEvent();
+        even.time = 0.5f;
+        even.functionName = "PrintEvent";
+        anim = this.GetComponent<Animation>();
+        state = anim["idle"];
+        clip = anim.clip;
+        clip.AddEvent(even);
+    }
+    public void PrintEvent() {
+        Debug.Log(state.time);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
