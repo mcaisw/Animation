05f7cfa578c1940c6056d56c77501374389b1d68
MengChi <mc2311362372@gmail.com>
2017-08-25 16:20:27 +0800

    ··

---
+++ Assets/Script/CountTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountTime : MonoBehaviour {
+    TimeSpan hours = new TimeSpan(2,0,0);
+    DateTime CurrentTime;
+    DateTime disPlayTime;
+    Text temp;
+    // Use this for initialization
+    void Start () {
+        temp = GetComponent<Text>();
+        CurrentTime = DateTime.Now.Add(hours);
+        //StartCoroutine(CountTimer());
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //temp.text = (CurrentTime - DateTime.Now).ToString();
+        temp.text = string.Format("{0:D2}:{1:D2}:{2:D2}", (CurrentTime - DateTime.Now).Hours, (CurrentTime - DateTime.Now).Minutes, (CurrentTime - DateTime.Now).Seconds);
+    }
+
+    IEnumerator CountTimer() {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            //Debug.Log((CurrentTime - DateTime.Now));
+        }
+    }
+}
