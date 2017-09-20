a198c7c3b947478866dc7613aa2494dd7a8a34c4
MengChi <mc2311362372@gmail.com>
2017-09-20 10:38:12 +0800

    customYieldInstruction

---
+++ Assets/Script/WaitingEndOfAnimaTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingEndOfAnimaTest : MonoBehaviour {
+    Animation anima;
+    // Use this for initialization
+    void Start()
+    {
+        anima = GetComponent<Animation>();
+        StartCoroutine(WhileAnimaEnd());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator WhileAnimaEnd()
+    {
+        yield return new WaitingEndofAnimi(anima);
+        Debug.Log("播放完毕");
+    }
+}
---
+++ Assets/Script/WaitingEndofAnimi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingEndofAnimi : CustomYieldInstruction
+{
+    Animation animi;
+    public override bool keepWaiting
+    {
+        get
+        {
+            return animi.isPlaying;
+        }
+    }
+
+    public WaitingEndofAnimi(Animation clip) {
+        animi = clip;
+    }
+}
