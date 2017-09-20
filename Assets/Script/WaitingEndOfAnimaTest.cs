b4650f20715601c0f465600377877507ae945abc
MengChi <mc2311362372@gmail.com>
2017-09-20 18:02:47 +0800

    ··

--- Assets/Script/WaitingEndOfAnimaTest.cs
+++ Assets/Script/WaitingEndOfAnimaTest.cs
@@ -14,12 +14,23 @@ public class WaitingEndOfAnimaTest : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        if (anima["attack"].normalizedTime >= 0.97f)//设置循环播放攻击动画
+        {
+            anima["attack"].normalizedTime = 0;
+            StartCoroutine(Attack());
+        }
     }
 
     IEnumerator WhileAnimaEnd()
     {
         yield return new WaitingEndofAnimi(anima);
         Debug.Log("播放完毕");
+    }
+    
+
+    IEnumerator Attack()
+    {
+        yield return new WaitWhile(() => anima["attack"].normalizedTime * 19 <= 10);//当动画帧数在10帧之前，悬挂到这里
+        Debug.Log("attack");
     }
 }
